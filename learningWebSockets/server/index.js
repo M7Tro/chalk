aac42b5ce3ea8da517a26fd0fd89bd3884423351
M7Tr0_ <mechanozver@gmail.com>
2024-09-23 22:59:36 +0800

    set up the backend. It uses three different packages at once: express, http and socket.io"

---
+++ learningWebSockets/server/index.js
@@ -0,0 +1,38 @@
+/* const express = require('express');
+const app = express();
+const http = require('http');
+const {Server} = require("socket.io"); //grabbing  the Server class from socket.io
+const cors = require('cors');
+app.use(cors()); //middleware necessary to accept the cors settings
+
+
+const server = http.createServer(app); //Creating an http server with an express app 
+const io = new Server(server, { //This is what we use to implement the socket.io functionality
+    cors: {
+        origin: "http://localhost:3000", //where the frontend runs
+        methods: ["POST", "GET"]
+    }
+}) 
+
+server.listen(3001, ()=>{ //listening on a port other than the one used by the React app
+    console.log("Server is running");
+}) */
+
+
+const express = require('express');
+const cors = require('cors');
+const http = require("http");
+const {Server} = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+server.listen(3001, () => {
+    console.log("Server listening on port", 3001);
+})
