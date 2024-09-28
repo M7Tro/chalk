cb010c4068da9e906d37e9d907667c58ab64ca4a
M7Tr0_ <mechanozver@gmail.com>
2024-09-28 21:23:50 +0800

    now you can draw. Hoooraygit add .

--- frontend/script.js
+++ frontend/script.js
@@ -4,24 +4,38 @@ let context;
 let canvasX;
 let canvasY;
 
+let mousedown = false;
+
 window.addEventListener("load", () =>{
     context = canvas.getContext("2d");
 
+    //This is necessary for proper function of the canvas. If width and height are specified only with CSS, canvas will be stretched
     let rect = canvas.getBoundingClientRect();
     canvas.setAttribute("width", rect.width);
     canvas.setAttribute("height", rect.height);
+})
 
-    if(context){
-        context.beginPath();
-        context.moveTo(50, 50);
-        context.lineTo(100, 100);
+canvas.addEventListener("mousedown", (e) => {
+    mousedown = true;
+
+    canvasX = e.clientX - canvas.getBoundingClientRect().x;
+    canvasY = e.clientY - canvas.getBoundingClientRect().y;
+    
+    context.beginPath();
+    context.moveTo(canvasX, canvasY);
+})
+
+canvas.addEventListener("mousemove", (e) => {
+    if(mousedown){
+        canvasX = e.clientX - canvas.getBoundingClientRect().x;
+        canvasY = e.clientY - canvas.getBoundingClientRect().y;
+
+        context.lineTo(canvasX, canvasY);
         context.stroke();
     }
 })
 
-canvas.addEventListener('click', (e) =>{
-    e.preventDefault();
-    canvasX = e.clientX - canvas.getBoundingClientRect().x;
-    canvasY = e.clientY - canvas.getBoundingClientRect().y;
-    console.log(canvasX, canvasY);
+canvas.addEventListener("mouseup", (e) => {
+    mousedown = false;
+    context.closePath();
 })
