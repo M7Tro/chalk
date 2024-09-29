34028e28f4158388dcddff681193a46feafdfe9b
M7Tr0_ <mechanozver@gmail.com>
2024-09-29 23:08:58 +0800

    added password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         minLength: 6,
     }
+})
+
+userSchema.pre("save", async function(next){
+    const document = this; //The document being saved 
+    const salt = await bcrypt.genSalt(10);
+    document.password = await bcrypt.hash(this.password, salt);
+    next();
 })
 
 const User = mongoose.model("User", userSchema);
