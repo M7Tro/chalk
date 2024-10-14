1f2c49c948e40490933630be3f3cf823fa0929a8
M7Tr0_ <mechanozver@gmail.com>
2024-10-14 18:21:12 +0800

    implemented saveImage endpoint

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -3,9 +3,12 @@ const Image = require('../models/Image.js');
 const saveImage = async (req, res) => {
     try{
         const {username, type, data} = JSON.parse(req.body);
-        const image = await Image.findOne({username});
-        if(image){
-            const newImage = await Image.
+        const image = await Image.replaceOne({username}, {username, type, data});
+        if(image.modifiedCount == 0){
+            const newImage = await Image.create({username, type, data});
+            res.status(200).json({savedImage: newImage});
+        }else{
+            res.status(200).json({savedImage: image});
         }
     }catch(err){
         console.log("Error while saving image:", err.message);
